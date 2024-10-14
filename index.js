fe4be9898a14d92608f54b3470c06d5df0df776d
isaacbrds <isaac.brigido@gmail.com>
2024-10-14 19:23:56 -0300

    feat: Finalizando Desafio

---
+++ index.js
@@ -0,0 +1,28 @@
+let nomeHeroi = "João das Bolachas";
+let pontosDeExperiencia = 12000;
+
+if (pontosDeExperiencia <= 1000){
+    nivelDoHeroi = "Ferro"
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivelDoHeroi}`)
+}else if (pontosDeExperiencia <= 2000){
+    nivelDoHeroi = "Bronze"
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivelDoHeroi}`)
+}else if (pontosDeExperiencia <= 5000){
+    nivelDoHeroi = "Prata"
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivelDoHeroi}`)
+}else if (pontosDeExperiencia <= 7000){
+    nivelDoHeroi = "Ouro"
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivelDoHeroi}`)
+}else if (pontosDeExperiencia <= 8000){
+    nivelDoHeroi = "Platina"
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivelDoHeroi}`)
+}else if (pontosDeExperiencia <= 9000){
+    nivelDoHeroi = "Acendente"
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivelDoHeroi}`)
+}else if (pontosDeExperiencia <= 10000){
+    nivelDoHeroi = "Imortal"
+    console.log(`O Herói de nome ${nomeHeroi} está no nível ${nivelDoHeroi}`)
+}else if (pontosDeExperiencia > 10000){
+    nivelDoHeroi = "Radiante"
+    console.log(`O Herói de nome **${nomeHeroi}** está no nível **${nivelDoHeroi}**`)
+}
